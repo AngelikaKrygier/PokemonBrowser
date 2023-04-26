8ed31cf0f6d9500966d712675d28a667b1c9e206
Angelika Krygier <angelika.krygier96@gmail.com>
2023-04-26 15:14:28 +0200

    added Container for the entire application

---
+++ src/common/Container/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+margin: 0 auto;
+max-width: 1000px;
+padding: ${({ theme }) => theme.padding.small};
+background-color: ${({ theme }) => theme.container.background};
+box-shadow: ${({ theme }) => theme.boxShadow.basic};
+`;
