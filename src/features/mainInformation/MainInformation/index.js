d166d3aa90e57e41eb179d884e94c3ad0128c373
Angelika Krygier <angelika.krygier96@gmail.com>
2023-04-27 13:55:10 +0200

    added content to MainInformation component

--- src/features/mainInformation/MainInformation/index.js
+++ src/features/mainInformation/MainInformation/index.js
@@ -1,3 +1,12 @@
+import { Tile } from "../../../common/Tile";
+import pokemonImages from "../images/pokemon.jpg"
+
 export const MainInformation = () => (
-<></>
+    <Tile
+        name="Pokemony"
+        img={pokemonImages}
+        extraDescription="Pokémon jest to japońska franczyza rozwijana od 1996 roku, na którą składają się produkcje, w których pojawiają się fikcyjne istoty, zwane Pokémonami. Najpopularniejszymi dziełami z franczyzy są główna seria gier (Pocket Monsters Red i Green oraz jej kontynuacje) i serial anime Pokémon. Oprócz tego istnieją też inne gry i seriale o Pokémonach, a także gra karciana Pokémon Trading Card Game, mangi na podstawie gier i serialu, ponad 20 pełnometrażowych filmów anime na podstawie serialu i aktorski film pełnometrażowy."
+        content="Przejdż do Pokemonów"
+    >
+    </Tile>
 );
