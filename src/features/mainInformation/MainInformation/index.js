5e0ca29ea98d9bef1730642cd9b21f61c9f1bb16
Angelika Krygier <angelika.krygier96@gmail.com>
2023-05-04 12:01:17 +0200

    fixed bug in MianInformation component

--- src/features/mainInformation/MainInformation/index.js
+++ src/features/mainInformation/MainInformation/index.js
@@ -5,8 +5,14 @@ export const MainInformation = () => (
     <Tile
         name="Pokemony"
         img={pokemonImages}
-        extraDescription="Pokémon jest to japońska franczyza rozwijana od 1996 roku, na którą składają się produkcje, w których pojawiają się fikcyjne istoty, zwane Pokémonami. Najpopularniejszymi dziełami z franczyzy są główna seria gier (Pocket Monsters Red i Green oraz jej kontynuacje) i serial anime Pokémon. Oprócz tego istnieją też inne gry i seriale o Pokémonach, a także gra karciana Pokémon Trading Card Game, mangi na podstawie gier i serialu, ponad 20 pełnometrażowych filmów anime na podstawie serialu i aktorski film pełnometrażowy."
-        content="Przejdż do Pokemonów"
+        extraContent="Pokémon jest to japońska franczyza rozwijana od 1996 roku, 
+        na którą składają się produkcje, w których pojawiają się fikcyjne istoty, 
+        zwane Pokémonami. Najpopularniejszymi dziełami z franczyzy są główna 
+        seria gier (Pocket Monsters Red i Green oraz jej kontynuacje) i serial 
+        anime Pokémon. Oprócz tego istnieją też inne gry i seriale o Pokémonach, 
+        a także gra karciana Pokémon Trading Card Game, mangi na podstawie gier 
+        i serialu, ponad 20 pełnometrażowych filmów anime na podstawie serialu 
+        i aktorski film pełnometrażowy."
     >
     </Tile>
 );
